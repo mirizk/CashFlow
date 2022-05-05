808332e21900384f5bbaab534c245bc53a48ee96
avishagbe <avishagbz17@gmail.com>
2022-05-05 11:02:02 +0300

    add glide

--- app/src/main/java/com/mityaalim/ui/main/investment/ServiceViewHolder.kt
+++ app/src/main/java/com/mityaalim/ui/main/investment/ServiceViewHolder.kt
@@ -1,8 +1,8 @@
 package com.mityaalim.ui.main.investment
 
-//import com.bumptech.glide.Glide
-//import com.bumptech.glide.load.MultiTransformation
-//import com.bumptech.glide.load.resource.bitmap.CenterCrop
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.MultiTransformation
+import com.bumptech.glide.load.resource.bitmap.CenterCrop
 import com.maatayim.koren.ui.base.list.BaseViewHolder
 import com.mityaalim.databinding.ItemServiceBinding
 
@@ -14,16 +14,16 @@ class ServiceViewHolder(
         item.price?.let {
             binding.price.text = it.toString()
         }
-//        item.imageUrl?.let {
-//            Glide
-//                .with(binding.root.context)
-//                .load(item.imageUrl)
-//                .transform(
-//                    MultiTransformation(
-//                        CenterCrop(),
-//                    )
-//                )
-//                .into(binding.background)
-//        }
+        item.imageUrl?.let {
+            Glide
+                .with(binding.root.context)
+                .load(item.imageUrl)
+                .transform(
+                    MultiTransformation(
+                        CenterCrop(),
+                    )
+                )
+                .into(binding.background)
+        }
     }
 }
