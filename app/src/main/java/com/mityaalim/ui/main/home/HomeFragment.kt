18ffd95d4b7e9562a016dd6ada1d09b9788597de
mirizk <65334156+mirizk@users.noreply.github.com>
2022-11-28 13:08:03 +0200

    the daily tip

--- app/src/main/java/com/mityaalim/ui/main/home/HomeFragment.kt
+++ app/src/main/java/com/mityaalim/ui/main/home/HomeFragment.kt
@@ -7,8 +7,11 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import com.mityaalim.R
 import com.mityaalim.databinding.FragmentHomeBinding
 import dagger.hilt.android.AndroidEntryPoint
+import java.time.DayOfWeek
+import java.time.LocalDateTime
 
 @AndroidEntryPoint
 class HomeFragment : Fragment() {
@@ -39,6 +42,16 @@ class HomeFragment : Fragment() {
         }
         binding.investmentBackground.setOnClickListener {
             findNavController().navigate(HomeFragmentDirections.actionHomeFragmentToInvestmentFragment())
+        }
+
+        binding.tipContent.text = when(LocalDateTime.now().dayOfWeek){
+            DayOfWeek.SUNDAY-> getString(R.string.the_first_tip)
+            DayOfWeek.MONDAY-> getString(R.string.the_second_tip)
+            DayOfWeek.TUESDAY-> getString(R.string.the_third_tip)
+            DayOfWeek.WEDNESDAY-> getString(R.string.the_fourth_tip)
+            DayOfWeek.THURSDAY-> getString(R.string.the_fifth_tip)
+            DayOfWeek.FRIDAY-> getString(R.string.the_sixth_tip)
+            DayOfWeek.SATURDAY -> getString(R.string.the_seventh_tip)
         }
     }
 }
